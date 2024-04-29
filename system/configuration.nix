f40310d4f230d0eaf01c611ba682c54be7dd0be8
William Fish <william.michael.fish@gmail.com>
2024-04-29 22:32:52 +0100

    .

--- system/configuration.nix
+++ system/configuration.nix
@@ -173,7 +173,6 @@
 
   system.stateVersion = "23.11";
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   fonts = {
     packages = with pkgs; [
@@ -200,5 +199,23 @@
     extraConfig = ''
         DefaultTimeoutStopSec=10s
     '';
+  };
+
+  documentation.nixos.enable = false;
+
+  nixpkgs.config.allowUnfree = true;
+
+  nix = {
+    settings = {
+      warn-dirty = false;
+      experimental-features = [ "nix-command" "flakes" ];
+      auto-optimise-store = true;
+    };
+
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
   };
 }
