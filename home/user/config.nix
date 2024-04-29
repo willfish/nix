c34c58ea0e430b37455f19866325921aff767e82
William Fish <william.michael.fish@gmail.com>
2024-04-29 23:26:45 +0100

    .

--- home/user/config.nix
+++ home/user/config.nix
@@ -3,13 +3,12 @@ in
 {
   home.file = {
       ".config/kitty".source = "${configDir}/kitty";
-      # ".config/neofetch".source = "${configDir}/neofetch";
       ".config/hypr".source = "${configDir}/hypr";
       ".config/swayidle".source = "${configDir}/swayidle";
       ".config/swaylock".source = "${configDir}/swaylock";
       ".config/wlogout".source = "${configDir}/wlogout";
       ".config/waybar".source = "${configDir}/waybar";
-      # ".config/btop".source = "${configDir}/btop";
+      ".config/btop".source = "${configDir}/btop";
       ".config/wofi".source = "${configDir}/wofi";
       ".config/mako".source = "${configDir}/mako";
   };
