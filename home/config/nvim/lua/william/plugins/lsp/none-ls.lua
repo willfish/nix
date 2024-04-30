7d67c52a261b8548fa262e7bae2676eb5fc4a493
William Fish <william.michael.fish@gmail.com>
2024-04-30 12:36:03 +0100

    .

--- home/config/nvim/lua/william/plugins/lsp/none-ls.lua
+++ home/config/nvim/lua/william/plugins/lsp/none-ls.lua
@@ -63,36 +63,36 @@ return {
       -- configure format on save
       on_attach = function(current_client, bufnr)
         -- filter down to only the keymap we're interested in
-        norm_maps = vim.tbl_filter(function(v)
-            return v.lhs == "<leader>a"
-        end, norm_maps)
-
-        if #norm_maps == 0 then
-            vim.keymap.set(
-                "n",
-                "<leader>a",
-                function()
-                    vim.lsp.buf.format { async = true }
-                end,
-                { noremap = true, silent = true }
-            )
-        end
-        if current_client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format({
-                filter = function(client)
-                  --  only use null-ls for formatting instead of lsp server
-                  return client.name == "null-ls"
-                end,
-                bufnr = bufnr,
-              })
-            end,
-          })
-        end
+        -- norm_maps = vim.tbl_filter(function(v)
+        --     return v.lhs == "<leader>a"
+        -- end, norm_maps)
+        --
+        -- if #norm_maps == 0 then
+        vim.keymap.set(
+          "n",
+          "<leader>a",
+          function()
+            vim.lsp.buf.format { async = true }
+          end,
+          { noremap = true, silent = true }
+        )
+        -- end
+        -- if current_client.supports_method("textDocument/formatting") then
+        --   vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+        --   vim.api.nvim_create_autocmd("BufWritePre", {
+        --     group = augroup,
+        --     buffer = bufnr,
+        --     callback = function()
+        --       vim.lsp.buf.format({
+        --         filter = function(client)
+        --           --  only use null-ls for formatting instead of lsp server
+        --           return client.name == "null-ls"
+        --         end,
+        --         bufnr = bufnr,
+        --       })
+        --     end,
+        --   })
+        -- end
       end,
     })
   end,
