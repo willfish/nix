f33e4b0e824175b3794f7d3c81bff70e4800c538
William Fish <william.michael.fish@gmail.com>
2024-04-30 12:48:19 +0100

    .

--- home/config/nvim/lua/william/plugins/linting.lua
+++ home/config/nvim/lua/william/plugins/linting.lua
@@ -6,12 +6,12 @@ return {
     local lint = require("lint")
 
     lint.linters_by_ft = {
-      javascript = { "eslint_d" },
-      typescript = { "eslint_d" },
-      javascriptreact = { "eslint_d" },
-      typescriptreact = { "eslint_d" },
-      svelte = { "eslint_d" },
-      python = { "pylint" },
+      javascript = { "eslint" },
+      typescript = { "eslint" },
+      javascriptreact = { "eslint" },
+      typescriptreact = { "eslint" },
+      svelte = { "eslint" },
+      python = { "pyright" },
     }
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
--- home/config/nvim/lua/william/plugins/lsp/none-ls.lua
+++ home/config/nvim/lua/william/plugins/lsp/none-ls.lua
@@ -80,23 +80,6 @@ return {
                 { noremap = true, silent = true }
             )
         end
-        -- end
-        -- if current_client.supports_method("textDocument/formatting") then
-        --   vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        --   vim.api.nvim_create_autocmd("BufWritePre", {
-        --     group = augroup,
-        --     buffer = bufnr,
-        --     callback = function()
-        --       vim.lsp.buf.format({
-        --         filter = function(client)
-        --           --  only use null-ls for formatting instead of lsp server
-        --           return client.name == "null-ls"
-        --         end,
-        --         bufnr = bufnr,
-        --       })
-        --     end,
-        --   })
-        -- end
       end,
     })
   end,
