bdd8d4685cd400f88de70a86ee3f4b6ab0f1030e
William Fish <william.michael.fish@gmail.com>
2024-04-30 12:03:16 +0100

    .

--- home/config/nvim/lua/william/plugins/telescope.lua
+++ home/config/nvim/lua/william/plugins/telescope.lua
@@ -4,7 +4,11 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    "nvim-telescope/telescope-github.nvim",
+    "xiyaowong/telescope-emoji.nvim",
 		"nvim-tree/nvim-web-devicons",
+    "nvim-telescope/telescope-live-grep-args.nvim",
+    "tsakirist/telescope-lazy.nvim",
 	},
 	config = function()
 		local telescope = require("telescope")
