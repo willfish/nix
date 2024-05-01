a3d781e757bcacf8635ae02d835fe1925d8a456a
William Fish <william.michael.fish@gmail.com>
2024-05-01 11:39:59 +0100

    .

--- home/config/nvim/lua/william/plugins/lsp/none-ls.lua
+++ home/config/nvim/lua/william/plugins/lsp/none-ls.lua
@@ -14,48 +14,41 @@ return {
 
     mason_null_ls.setup({
       ensure_installed = {
-        "prettier", -- prettier formatter
-        "stylua", -- lua formatter
         "black", -- python formatter
+        "eslint_d", -- js linter
+        "golangci_lint", -- golang linter
+        "markdownlint", -- markdown linter
         "pylint", -- python linter
-        "eslint-lsp", -- js linter
+        "rubyfmt", -- ruby formatter
+        "shellharden", -- shell formatter
+        "shfmt", -- shell formatter
+        "stylua", -- lua formatter
       },
     })
 
     -- for conciseness
     local formatting = null_ls.builtins.formatting -- to setup formatters
     local diagnostics = null_ls.builtins.diagnostics -- to setup linters
+    local codeactions = null_ls.builtins.code_actions -- to setup linters
 
     -- configure null_ls
     null_ls.setup({
-      -- add package.json as identifier for root (for typescript monorepos)
-      -- root_dir = null_ls_utils.root_pattern(".null-ls-root", "Makefile", ".git", "package.json"),
-      -- setup formatters & linters
       sources = {
-        diagnostics.erb_lint,
-        diagnostics.eslint,
-        diagnostics.fish,
-        diagnostics.flake8,
         diagnostics.golangci_lint,
         diagnostics.markdownlint,
-        formatting.autopep8,
         formatting.black,
-        formatting.erb_lint,
-        formatting.fish_indent,
         formatting.hclfmt,
-        formatting.isort,
         formatting.markdownlint,
+        formatting.rubyfmt,
         formatting.shellharden,
-        formatting.sql_formatter,
-        formatting.stylua, -- lua formatter
-        formatting.trim_whitespace,
+        formatting.stylua,
         formatting.xmllint,
 
-        -- diagnostics.eslint_d.with({ -- js/ts linter
-        --   condition = function(utils)
-        --     return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs" }) -- only enable if root has .eslintrc.js or .eslintrc.cjs
-        --   end,
-        -- }),
+        diagnostics.eslint_d.with({
+          condition = function(utils)
+            return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs" })
+          end,
+        }),
       },
       -- configure format on save
       on_attach = function(current_client, bufnr)
