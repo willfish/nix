9bc342264a59e6e196916cdadfc889fa1ca481e1
William Fish <william.michael.fish@gmail.com>
2024-05-19 08:59:07 +0100

    .

--- system/configuration.nix
+++ system/configuration.nix
@@ -55,6 +55,12 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+    openFirewall = true;
+  };
+
   hardware.system76.enableAll = true;
   # Enable sound with pipewire.
   sound.enable = true;
