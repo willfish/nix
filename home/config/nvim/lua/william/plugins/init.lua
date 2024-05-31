43a1b3a41e10b9bc1737157bf55c55c9b0518302
William Fish <william.michael.fish@gmail.com>
2024-05-31 10:56:01 +0100

    .

--- home/config/nvim/lua/william/plugins/init.lua
+++ home/config/nvim/lua/william/plugins/init.lua
@@ -31,5 +31,6 @@ return {
   'fadein/vim-FIGlet',
   'sotte/presenting.vim',
   'xiyaowong/telescope-emoji.nvim',
-  'stefandtw/quickfix-reflector.vim'
+  'stefandtw/quickfix-reflector.vim',
+  'lepture/vim-jinja'
 }
