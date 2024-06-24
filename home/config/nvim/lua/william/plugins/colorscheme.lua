78c11d6d1873c8fe5e23294fa9da0f6dc7565431
William Fish <william.michael.fish@gmail.com>
2024-06-24 09:45:56 +0100

    .

--- home/config/nvim/lua/william/plugins/colorscheme.lua
+++ home/config/nvim/lua/william/plugins/colorscheme.lua
@@ -1,17 +1,14 @@
 return {
-
+  {
+    "catppuccin/nvim",
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      local catppuccin = require("catppuccin")
+      catppuccin.setup({
+        flavour = "macchiato"
+      })
+      -- load the colorscheme here
+      vim.cmd([[colorscheme catppuccin]])
+    end,
+  }
 }
--- return {
---   {
---     "catppuccin/nvim",
---     priority = 1000, -- make sure to load this before all the other start plugins
---     config = function()
---       local catppuccin = require("catppuccin")
---       catppuccin.setup({
---         flavour = "macchiato"
---       })
---       -- load the colorscheme here
---       vim.cmd([[colorscheme catppuccin]])
---     end,
---   }
--- }
