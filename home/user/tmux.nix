b1d79bd7cd2cee1b5ca3f922bced8fb1456df58e
William Fish <william.michael.fish@gmail.com>
2024-09-22 20:39:52 +0100

    .

--- home/user/tmux.nix
+++ home/user/tmux.nix
@@ -18,6 +18,9 @@
           set -g @dracula-show-battery false
           set -g @dracula-show-powerline true
           set -g @dracula-refresh-rate 10
+          set -g @dracula-show-fahrenheit false
+          set -g @dracula-fixed-location "Royal Tunbdige Wells, UK"
+          set -g @dracula-plugins "cpu-usage gpu-usage ram-usage weather git synchronize-panes"
         '';
       }
     ];
