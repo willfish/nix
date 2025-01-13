97bf1e34567b1fbf60c09c3ee05378f96938f0d4
William Fish <william.michael.fish@gmail.com>
2025-01-13 19:57:11 +0000

    .

--- home/config/nvim/lua/william/plugins/lsp/lspconfig.lua
+++ home/config/nvim/lua/william/plugins/lsp/lspconfig.lua
@@ -168,5 +168,10 @@ return {
       capabilities = capabilities,
       on_attach = on_attach,
     })
+
+    lspconfig["clangd"].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
   end,
 }
