8ed425fbb810f706b619f431af2f4931e9b70c35
William Fish <william.michael.fish@gmail.com>
2025-01-18 23:03:04 +0000

    .

--- home/user/config.nix
+++ home/user/config.nix
@@ -21,8 +21,7 @@ in
       ".pryrc".source = "${configDir}/pryrc";
       ".tmux/plugins/tmux-sessionx".source = "${configDir}/tmux/plugins/tmux-sessionx";
       ".wallpapers".source = "${configDir}/variety/Favorites";
-      ".config/ghostty/config".source = "${configDir}/ghostty/config";
-      ".config/ghostty/shaders".source = "${configDir}/ghostty/shaders";
+      ".config/ghostty".source = "${configDir}/ghostty";
   };
 
   systemd.user.timers.wallpaper = {
