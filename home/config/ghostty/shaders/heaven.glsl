9e6fc27fe8bb660eb1dc182a1f456ed08ea381a1
William Fish <william.michael.fish@gmail.com>
2025-01-18 23:26:44 +0000

    .

--- home/config/ghostty/shaders/heaven.glsl
+++ home/config/ghostty/shaders/heaven.glsl
@@ -1,115 +1,98 @@
-//---  Galaxy --- Fabrice NEYRET  august 2013
+// CC0: Another windows terminal shader
+//  Created this based on an old shader as a background in windows terminal
 
-const float RETICULATION = 3.;  // strenght of dust texture
-const float NB_ARMS = 5.;       // number of arms
-//const float ARM = 3.;         // contrast in/out arms
-const float COMPR = .1;         // compression in arms
-const float SPEED = .1;
-const float GALAXY_R = 1./2.;
-const float BULB_R = 1./4.;
-const vec3 GALAXY_COL = vec3(.9,.9,1.); //(1.,.8,.5);
-const vec3 BULB_COL   = vec3(1.,.8,.8);
-const vec3 SKY_COL    = .5*vec3(.1,.3,.5);
+#define TIME        iTime
+#define RESOLUTION  iResolution
+#define PI          3.141592654
+#define TAU         (2.0*PI)
+#define ROT(a)      mat2(cos(a), sin(a), -sin(a), cos(a))
 
-#define t iTime
+// License: WTFPL, author: sam hocevar, found: https://stackoverflow.com/a/17897228/418488
+const vec4 hsv2rgb_K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+vec3 hsv2rgb(vec3 c) {
+  vec3 p = abs(fract(c.xxx + hsv2rgb_K.xyz) * 6.0 - hsv2rgb_K.www);
+  return c.z * mix(hsv2rgb_K.xxx, clamp(p - hsv2rgb_K.xxx, 0.0, 1.0), c.y);
+}
+// License: WTFPL, author: sam hocevar, found: https://stackoverflow.com/a/17897228/418488
+//  Macro version of above to enable compile-time constants
+#define HSV2RGB(c)  (c.z * mix(hsv2rgb_K.xxx, clamp(abs(fract(c.xxx + hsv2rgb_K.xyz) * 6.0 - hsv2rgb_K.www) - hsv2rgb_K.xxx, 0.0, 1.0), c.y))
 
-// --- base noise
-float tex(vec2 uv)
-{
-	float n = texture(iChannel0,uv, 0.).r;
+const mat2 rot0 = ROT(0.0);
+mat2 g_rot0 = rot0;
+mat2 g_rot1 = rot0;
 
-#define MODE 3  // kind of noise texture
-#if MODE==0         // unsigned
-	#define A 2.
-	return n;
-#elif MODE==1       // signed
-	#define A 3.
-	return 2.*n-1.;
-#elif MODE==2       // bulbs
-	#define A 3.
-	return abs(2.*n-1.);
-#elif MODE==3       // wires
-	#define A 1.5
-	return 1.-abs(2.*n-1.);
-#endif
+// License: Unknown, author: nmz (twitter: @stormoid), found: https://www.shadertoy.com/view/NdfyRM
+vec3 sRGB(vec3 t) {
+  return mix(1.055*pow(t, vec3(1./2.4)) - 0.055, 12.92*t, step(t, vec3(0.0031308)));
+}
+
+// License: Unknown, author: Matt Taylor (https://github.com/64), found: https://64.github.io/tonemapping/
+vec3 aces_approx(vec3 v) {
+  v = max(v, 0.0);
+  v *= 0.6f;
+  float a = 2.51f;
+  float b = 0.03f;
+  float c = 2.43f;
+  float d = 0.59f;
+  float e = 0.14f;
+  return clamp((v*(a*v+b))/(v*(c*v+d)+e), 0.0f, 1.0f);
+}
+
+float apolloian(vec3 p, float s) {
+  float scale = 1.0;
+  for(int i=0; i < 5; ++i) {
+    p = -1.0 + 2.0*fract(0.5*p+0.5);
+    float r2 = dot(p,p);
+    float k  = s/r2;
+    p       *= k;
+    scale   *= k;
+  }
+
+  vec3 ap = abs(p/scale);
+  float d = length(ap.xy);
+  d = min(d, ap.z);
+
+  return d;
 }
 
 
-// --- perlin turbulent noise + rotation
-float noise(vec2 uv)
-{
-	float v=0.;
-	float a=-SPEED*t,	co=cos(a),si=sin(a);
-	mat2 M = mat2(co,-si,si,co);
-	const int L = 7;
-	float s=1.;
-	for (int i=0; i<L; i++)
-	{
-		uv = M*uv;
-		float b = tex(uv*s);
-		v += 1./s* pow(b,RETICULATION);
-		s *= 2.;
-	}
-
-    return v/2.;
+float df(vec2 p) {
+  float fz = mix(0.75, 1., smoothstep(-0.9, 0.9, cos(TAU*TIME/300.0)));
+  float z = 1.55*fz;
+  p /= z;
+  vec3 p3 = vec3(p,0.1);
+  p3.xz*=g_rot0;
+  p3.yz*=g_rot1;
+  float d = apolloian(p3, 1.0/fz);
+  d *= z;
+  return d;
 }
 
-bool keyToggle(int ascii)
-{
-	return (texture(iChannel2,vec2((.5+float(ascii))/256.,0.75)).x > 0.);
+vec3 effect(vec2 p, vec2 pp) {
+  g_rot0 = ROT(0.1*TIME);
+  g_rot1 = ROT(0.123*TIME);
+
+  float aa = 2.0/RESOLUTION.y;
+
+  float d = df(p);
+  const vec3 bcol0 = HSV2RGB(vec3(0.55, 0.85, 0.85));
+  const vec3 bcol1 = HSV2RGB(vec3(0.33, 0.85, 0.025));
+  vec3 col = 0.1*bcol0;
+  col += bcol1/sqrt(abs(d));
+  col += bcol0*smoothstep(aa, -aa, (d-0.001));
+
+  col *= smoothstep(1.5, 0.5, length(pp));
+
+  return col;
 }
 
-void mainImage( out vec4 fragColor, in vec2 fragCoord )
-{
-	vec2 uv = fragCoord.xy/iResolution.y-vec2(.8,.5);
-	vec3 col;
-
-	// spiral stretching with distance
-	float rho = length(uv); // polar coords
-	float ang = atan(uv.y,uv.x);
-	float shear = 2.*log(rho); // logarythmic spiral
-	float c = cos(shear), s=sin(shear);
-	mat2 R = mat2(c,-s,s,c);
-
-	// galaxy profile
-	float r; // disk
-	r = rho/GALAXY_R; float dens = exp(-r*r);
-	r = rho/BULB_R;	  float bulb = exp(-r*r);
-	float phase = NB_ARMS*(ang-shear);
-	// arms = spirals compression
-	ang = ang-COMPR*cos(phase)+SPEED*t;
-	uv = rho*vec2(cos(ang),sin(ang));
-	// stretched texture must be darken by d(new_ang)/d(ang)
-	float spires = 1.+NB_ARMS*COMPR*sin(phase);
-	// pires = mix(1.,sin(phase),ARM);
-	dens *= .7*spires;
-
-	// gaz texture
-	float gaz = noise(.09*1.2*R*uv);
-	float gaz_trsp = pow((1.-gaz*dens),2.);
-
-	// stars
-	//float a=SPEED*t, co=cos(a),si=sin(a);
-	//mat2 M = mat2(co,-si,si,co);
-	// adapt stars size to display resolution
-	float ratio = .8*iResolution.y/iChannelResolution[0].y;
-	float stars1 = texture(iChannel1,ratio*uv+.5, 0.).r, // M*uv
-	      stars2 = texture(iChannel0,ratio*uv+.5, 0.).r,
-		  stars = pow(1.-(1.-stars1)*(1.-stars2),5.);
-
-	//stars = pow(stars,5.);
-
-	// keybord controls (numbers)
-	if (keyToggle(49)) gaz_trsp = 1./1.7;
-	if (keyToggle(50)) stars = 0.;
-	if (keyToggle(51)) bulb = 0.;
-	if (keyToggle(52)) dens = .3*spires;
-
-	// mix all
-	col = mix(SKY_COL,
-			  gaz_trsp*(1.7*GALAXY_COL) + 1.2*stars,
-			  dens);
-	col = mix(col, 1.2*BULB_COL, bulb);
-
-	fragColor = vec4(col,1.);
+void mainImage(out vec4 fragColor, in vec2 fragCoord) {
+  vec2 q = fragCoord/RESOLUTION.xy;
+  vec2 p = -1. + 2. * q;
+  vec2 pp = p;
+  p.x *= RESOLUTION.x/RESOLUTION.y;
+  vec3 col = effect(p, pp);
+  col = aces_approx(col);
+  col = sqrt(col);
+  fragColor = vec4(col, 1.0);
 }
