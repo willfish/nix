b6fcdddc9eb8bb38e257348d601c8ba2f5c8c0a2
William Fish <william.michael.fish@gmail.com>
2025-01-23 22:37:47 +0000

    .

--- system/starfish/configuration.nix
+++ system/starfish/configuration.nix
@@ -20,6 +20,10 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
+  networking.extraHosts = ''
+    127.0.0.1 host.docker.internal
+  '';
+
   # Enable networking
   networking.networkmanager.enable = true;
 
