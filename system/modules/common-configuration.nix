f56e2a65df88e0b1f2475d343a0122a09e77a813
William Fish <william.michael.fish@gmail.com>
2025-02-14 20:11:14 +0000

    .

--- system/modules/common-configuration.nix
+++ system/modules/common-configuration.nix
@@ -225,33 +225,33 @@
   };
 
   systemd = {
-    user.services.polkit-gnome-authentication-agent-1 = {
-      description = "polkit-gnome-authentication-agent-1";
-      wantedBy = [ "graphical-session.target" ];
-      wants = [ "graphical-session.target" ];
-      after = [ "graphical-session.target" ];
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-      };
-    };
-    extraConfig = ''
-      DefaultTimeoutStopSec=10s
-    '';
+    # user.services.polkit-gnome-authentication-agent-1 = {
+    #   description = "polkit-gnome-authentication-agent-1";
+    #   wantedBy = [ "graphical-session.target" ];
+    #   wants = [ "graphical-session.target" ];
+    #   after = [ "graphical-session.target" ];
+    #   serviceConfig = {
+    #     Type = "simple";
+    #     ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+    #     Restart = "on-failure";
+    #     RestartSec = 1;
+    #     TimeoutStopSec = 10;
+    #   };
+    # };
+    # extraConfig = ''
+    #   DefaultTimeoutStopSec=10s
+    # '';
   };
 
-  environment.etc."polkit-1/rules.d/50-nopasswd.rules" = {
-    text = ''
-      polkit.addRule(function(action, subject) {
-          if (subject.isInGroup("wheel")) {
-              return polkit.Result.YES;
-          }
-      });
-    '';
-  };
+  # environment.etc."polkit-1/rules.d/50-nopasswd.rules" = {
+  #   text = ''
+  #     polkit.addRule(function(action, subject) {
+  #         if (subject.isInGroup("wheel")) {
+  #             return polkit.Result.YES;
+  #         }
+  #     });
+  #   '';
+  # };
 
   documentation.nixos.enable = false;
 
