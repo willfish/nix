396a5df6b742d18510461d4bbd3819c7ed000170
William Fish <william.michael.fish@gmail.com>
2025-02-18 20:42:24 +0000

    .

--- flake.nix
+++ flake.nix
@@ -13,7 +13,8 @@
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.11";
+
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
