577f7c617dfd0b8f797216e6824d9a1f0f0e875d
William Fish <william.michael.fish@gmail.com>
2025-02-24 09:00:33 +0000

    .

--- home/config/kickstart/init.lua
+++ home/config/kickstart/init.lua
@@ -403,7 +403,7 @@ require('lazy').setup({
       'nvim-lua/plenary.nvim',
       { -- If encountering errors, see telescope-fzf-native README for installation instructions
         'nvim-telescope/telescope-fzf-native.nvim',
-
+        'nvim-telescope/telescope-github.nvim',
         -- `build` is used to run some command when the plugin is installed/updated.
         -- This is only run then, not every time Neovim starts up.
         build = 'make',
@@ -431,8 +431,11 @@ require('lazy').setup({
       -- Enable Telescope extensions if they are installed
       pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
+      pcall(require("telescope").load_extension, 'gh')
 
       local builtin = require 'telescope.builtin'
+
+      vim.keymap.set("n", "<Leader>sp", ":lua require('telescope').extensions.gh.pull_request()<CR>", { desc = "[S]earch [P]ull requests" })
       vim.keymap.set('n', '<C-f>', builtin.find_files, { desc = 'Search Files' })
       vim.keymap.set('n', '<C-g>', builtin.live_grep, { desc = 'Search by Grep' })
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
