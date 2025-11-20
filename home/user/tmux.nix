576c9bdcd1b81761b370ef50b52809960689718d
William Fish <william.michael.fish@gmail.com>
2025-11-20 20:46:20 +0000

    .

--- home/user/tmux.nix
+++ home/user/tmux.nix
@@ -1,4 +1,16 @@
 { pkgs-unstable, ... }:
+let
+  tmux-pomodoro-plus = pkgs-unstable.tmuxPlugins.mkTmuxPlugin {
+    pluginName = "pomodoro";
+    version = "unstable-2023-10-20";
+    src = pkgs-unstable.fetchFromGitHub {
+      owner = "olimorris";
+      repo = "tmux-pomodoro-plus";
+      rev = "main";
+      sha256 = "sha256-your-computed-hash-here=";
+    };
+  };
+in
 {
   programs.tmux = {
     package = pkgs-unstable.tmux;
@@ -6,6 +18,7 @@
     clock24 = true;
     plugins = with pkgs-unstable.tmuxPlugins; [
       fzf-tmux-url
+      tmux-pomodoro-plus
       jump
       resurrect
       sensible
@@ -14,10 +27,13 @@
       {
         plugin = dracula;
         extraConfig = ''
+          set -g @dracula-terraform-label true
           set -g @dracula-show-battery false
           set -g @dracula-show-powerline true
           set -g @dracula-refresh-rate 10
-          set -g @dracula-plugins "git attached-clients"
+          set -g @dracula-show-fahrenheit false
+          set -g @dracula-fixed-location "Kent"
+          set -g @dracula-plugins "git cpu-usage ram-usage weather"
         '';
       }
     ];
