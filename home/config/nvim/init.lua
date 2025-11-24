77ce05a5aa4b950ae0e47e7981b78f9cf4b7a186
William Fish <william.michael.fish@gmail.com>
2025-11-24 09:16:55 +0000

    .

--- home/config/nvim/init.lua
+++ home/config/nvim/init.lua
@@ -293,28 +293,12 @@ require("lazy").setup({
 		event = "VeryLazy",
 		keys = {
 			{
-				"s",
+				"S",
 				mode = { "n", "x", "o" },
 				function()
 					require("flash").jump()
 				end,
 				desc = "Flash",
-			},
-			{
-				"S",
-				mode = { "n", "x", "o" },
-				function()
-					require("flash").treesitter()
-				end,
-				desc = "Flash Treesitter",
-			},
-			{
-				"r",
-				mode = "o",
-				function()
-					require("flash").remote()
-				end,
-				desc = "Remote Flash",
 			},
 		},
 		opts = { modes = { char = { enabled = true } } },
