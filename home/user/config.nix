69a3f2e3ab36b8419ba8f7109fef0dbd5e277758
William Fish <william.michael.fish@gmail.com>
2026-02-25 07:46:38 +0000

    Add COSMIC desktop config and remove GNOME gsettings
    Configure COSMIC compositor via Home Manager xdg.configFile:
    shortcuts, autotile, focus-follows-cursor, and active hint.

--- home/user/config.nix
+++ home/user/config.nix
@@ -15,4 +15,31 @@ in
     ".gitmessage".source = "${configDir}/gitmessage";
     ".pryrc".source = "${configDir}/pryrc";
   };
+
+  xdg.configFile = {
+    "cosmic/com.system76.CosmicSettings.Shortcuts/v1/custom" = {
+      source = "${configDir}/cosmic/shortcuts";
+      force = true;
+    };
+    "cosmic/com.system76.CosmicComp/v1/autotile" = {
+      source = "${configDir}/cosmic/autotile";
+      force = true;
+    };
+    "cosmic/com.system76.CosmicComp/v1/autotile_behavior" = {
+      source = "${configDir}/cosmic/autotile_behavior";
+      force = true;
+    };
+    "cosmic/com.system76.CosmicComp/v1/active_hint" = {
+      source = "${configDir}/cosmic/active_hint";
+      force = true;
+    };
+    "cosmic/com.system76.CosmicComp/v1/focus_follows_cursor" = {
+      source = "${configDir}/cosmic/focus_follows_cursor";
+      force = true;
+    };
+    "cosmic/com.system76.CosmicComp/v1/cursor_follows_focus" = {
+      source = "${configDir}/cosmic/cursor_follows_focus";
+      force = true;
+    };
+  };
 }
