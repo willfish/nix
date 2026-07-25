5678015a917b7d5bdd61b1e21da9ab428ad1ad5a
William Fish <william.michael.fish@gmail.com>
2026-07-25 22:09:35 +0100

    fix(nix): trust Numtide binary cache
    Issue: No ticket/issue

--- system/modules/common-configuration.nix
+++ system/modules/common-configuration.nix
@@ -157,9 +157,11 @@
     settings = {
       substituters = [
         "https://cache.nixos.org"
+        "https://cache.numtide.com"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
       ];
       accept-flake-config = false;
       require-sigs = true;
